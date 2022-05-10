c7ba213ac59a9572d0965b4f051f59bc5f318936
Lương Việt Hoàng <tcm4095@gmail.com>
2022-05-10 20:53:12 +0700

    Added xor_list_const_iterator

--- xor_list_iter.hpp
+++ xor_list_iter.hpp
@@ -9,7 +9,7 @@
 //TODO: make const_iterator
 
 template<typename T>
-class xor_list_iterator {
+class xor_base_iterator {
 public:
 	using iterator_category = std::bidirectional_iterator_tag;
 	using difference_type = std::ptrdiff_t;
@@ -17,15 +17,15 @@ public:
 	using pointer = T*;
 	using reference = T&;
 
-	using node = xor_list_node<T>;
+	using node = xor_list_node<std::remove_cv_t<T>>;
 	node *prev,*curr;
 
-	constexpr xor_list_iterator() : prev(nullptr), curr(nullptr) {}
-	constexpr xor_list_iterator(node* prev,node *curr) : prev(prev),curr(curr) {}
-	constexpr xor_list_iterator(const xor_list_iterator &other) : prev(other.prev),curr(other.curr) {}
+	constexpr xor_base_iterator() : prev(nullptr), curr(nullptr) {}
+	constexpr xor_base_iterator(node* prev,node *curr) : prev(prev),curr(curr) {}
+	constexpr xor_base_iterator(const xor_base_iterator &other) : prev(other.prev),curr(other.curr) {}
 
 	constexpr operator bool() const noexcept { return curr != nullptr; }
-	constexpr friend bool operator==(const xor_list_iterator &a, const xor_list_iterator &b) noexcept {
+	constexpr friend bool operator==(const xor_base_iterator &a, const xor_base_iterator &b) noexcept {
 		return a.curr==b.curr;
 	}
 
@@ -38,13 +38,13 @@ public:
 	}
 
 
-	constexpr xor_list_iterator& operator++() {
+	constexpr xor_base_iterator& operator++() {
 		node *tmp = curr;
 		curr = reinterpret_cast<node*>(get_xor(curr->both,prev));
 		prev = tmp;
 		return *this;
 	}
-	constexpr xor_list_iterator& operator--() {
+	constexpr xor_base_iterator& operator--() {
 		node *tmp = prev;
 		prev = reinterpret_cast<node*>(get_xor(prev->both, curr));
 		curr = tmp;
@@ -52,18 +52,44 @@ public:
 	}
 
 
-	constexpr xor_list_iterator operator++(int) {
+	constexpr xor_base_iterator operator++(int) {
 		auto res = *this;
 		++*this;
 		return res;
 	}
-	constexpr xor_list_iterator operator--(int) {
+	constexpr xor_base_iterator operator--(int) {
 		auto res = *this;
 		--*this;
 		return res;
 	}
 
+};
+
+template<typename T>
+class xor_list_iterator : public xor_base_iterator<T> {
+public:
+	using typename xor_base_iterator<T>::node;
+
+	constexpr xor_list_iterator() : xor_base_iterator<T>() {}
+	constexpr xor_list_iterator(node* prev,node *curr) : xor_base_iterator<T>(prev,curr) {}
+	constexpr xor_list_iterator(const xor_list_iterator &other) : xor_base_iterator<T>(other) {}
+
 	friend constexpr void swap(xor_list_iterator &a, xor_list_iterator &b) {
+		auto tmp = a;
+		a = b;
+		b = tmp;
+	}
+};
+
+template<typename T>
+class xor_list_const_iterator : public xor_base_iterator<const T> {
+public:
+	using typename xor_base_iterator<const T>::node;
+
+	constexpr xor_list_const_iterator() : xor_base_iterator<const T>() {}
+	constexpr xor_list_const_iterator(node* prev,node *curr) : xor_base_iterator<const T>(prev,curr) {}
+	constexpr xor_list_const_iterator(const xor_list_const_iterator &other) : xor_base_iterator<const T>(other) {}
+	friend constexpr void swap(xor_list_const_iterator &a, xor_list_const_iterator &b) {
 		auto tmp = a;
 		a = b;
 		b = tmp;
