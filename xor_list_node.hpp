0b58feb27e75fde58bf6f6359991ac6ed4feeefe
Hoàng Lương Việt <tcm4095@gmail.com>
2022-06-19 23:53:01 +0700

    Removed swap functions

--- xor_list_node.hpp
+++ xor_list_node.hpp
@@ -13,12 +13,6 @@ struct xor_list_node {
 
 	constexpr xor_list_node(const value_type &value, uintptr_t both = 0) : value(value), both(both) {}
 	constexpr xor_list_node(value_type &&value, uintptr_t both = 0) : value(std::move(value)), both(both) {}
-
-	friend constexpr void swap(xor_list_node *&a, xor_list_node *&b) {
-		auto tmp = a;
-		a = b;
-		b = tmp;
-	}
 };
 
 #endif
